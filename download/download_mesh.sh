8db2d3f492d1ab212bbe8fee400bd578bbe1addc
verena <vpchung@users.noreply.github.com>
2020-09-19 00:42:54 -0700

    whoops, forgot to save the changes

--- download/download_mesh.sh
+++ download/download_mesh.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-# download mesh files from
 
 mkdir -p downloads/mesh_files
 cd downloads/mesh_files/
 
-#remove previous downloads
+# Remove previous downloads to save space
 rm d20*
 rm current_mesh
 
-#wget ftp://nlmpubs.nlm.nih.gov/online/mesh/2019/asciimesh/d20*.bin
 myyear=`date +'%Y'`
 wget ftp://nlmpubs.nlm.nih.gov/online/mesh/MESH_FILES/asciimesh/d20*.bin
 
 # Link to download
-
 ln -s d20* current_mesh
